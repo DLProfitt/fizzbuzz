e22cd9b83f581425c293a25e51674437dd6fc84c
David Profitt <dlprofitt@gmail.com>
2023-05-25 21:23:21 -0400

    translated JS code to C# successfully resulting in correct fizzbuzz game results through 100

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main()
+    {
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+            }
+    }
+}
